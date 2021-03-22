15901b778835927ea197a50cdf83bacd9219636e
Muhammad Nazih Mahmudi <nazih.mahmudi12@gmali.com>
2021-03-22 14:13:03 +0700

    Intent

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     EditText edEmail,edPassword;
     Button btSignIn;
 
-    String email,pwd;
+    String email,pass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,32 +28,44 @@ public class MainActivity extends AppCompatActivity {
         btSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                email = "admin1@mail.com";
-                pwd = "123";
 
-                if (email.equals(edEmail.getText().toString()) && pwd.equals(edPassword.getText().toString())) {
+                email = edEmail.getText().toString();
+                pass = edPassword.getText().toString();
 
+                String eml = "admin@mail.com";
+                String pwd = "123";
+
+                if (email.isEmpty() || pass.isEmpty()) {
                     Toast t = Toast.makeText(getApplicationContext(),
-                            "Login Sukses", Toast.LENGTH_LONG);
+                            "Email atau Password Tidak Boleh Kosong!", Toast.LENGTH_LONG);
                     t.show();
-
-                } else if (email.equals(edEmail.getText().toString())  && !pwd.equals(edPassword.getText().toString())) {
-
-                    Toast t = Toast.makeText(getApplicationContext(),
-                            "Password Salah", Toast.LENGTH_LONG);
-                    t.show();
-
-                } else if (!email.equals(edEmail.getText().toString()) && pwd.equals(edPassword.getText().toString())) {
-
-                    Toast t = Toast.makeText(getApplicationContext(),
-                            "Email Salah", Toast.LENGTH_LONG);
-                    t.show();
-
                 } else {
 
-                    Toast t = Toast.makeText(getApplicationContext(),
-                            "Email dan Password Salah", Toast.LENGTH_LONG);
-                    t.show();
+                    if (email.equals(eml) && pass.equals(pwd)) {
+
+                        Toast t = Toast.makeText(getApplicationContext(),
+                                "Login Sukses", Toast.LENGTH_LONG);
+                        t.show();
+
+                        Bundle b = new Bundle();
+
+                        b.putString("a", email.trim());
+
+                        b.putString("b", pass.trim());
+
+                        Intent i = new Intent(getApplicationContext(), ActivityHasil.class);
+
+                        i.putExtras(b);
+
+                        startActivity(i);
+
+                    } else {
+
+                        Toast t = Toast.makeText(getApplicationContext(),
+                                "Email atau Password Salah", Toast.LENGTH_LONG);
+                        t.show();
+
+                    }
 
                 }
 
