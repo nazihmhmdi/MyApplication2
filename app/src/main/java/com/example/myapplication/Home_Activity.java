455d1b5a3cb3db4b35d3ccd78be6d9b7988542c0
Muhammad Nazih Mahmudi <nazih.mahmudi12@gmali.com>
2021-04-20 10:32:50 +0700

    Menu

--- app/src/main/java/com/example/myapplication/Home_Activity.java
+++ app/src/main/java/com/example/myapplication/Home_Activity.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.PopupMenu;
+import android.widget.SearchView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -21,11 +22,15 @@ public class Home_Activity extends AppCompatActivity implements PopupMenu.OnMenu
 
     String[] listNama;
 
+    String nama;
+
     public static ArrayList<ClassNama> classNamaArrayList = new ArrayList<ClassNama>();
 
     Bundle bundle = new Bundle();
 
     Intent intent;
+
+    private SearchView sCari;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,10 +55,27 @@ public class Home_Activity extends AppCompatActivity implements PopupMenu.OnMenu
 
         list.setAdapter(adapter);
 
+        SearchView searchView = findViewById(R.id.search);
+        this.sCari = searchView;
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String nama) {
+                adapter.filter(nama);
+                searchView.clearFocus();
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String nextText) {
+                adapter.filter(nextText);
+                return false;
+            }
+        });
+
         list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                String nama = classNamaArrayList.get(position).getName();
+                nama = classNamaArrayList.get(position).getName();
 
                 bundle.putString("a",nama.trim());
 
@@ -70,18 +92,58 @@ public class Home_Activity extends AppCompatActivity implements PopupMenu.OnMenu
 
     @Override
     public boolean onMenuItemClick(MenuItem menuItem) {
-        switch (menuItem.getItemId())
-        {
+        switch (menuItem.getItemId()) {
             case R.id.mndata:
-                intent = new Intent(getApplicationContext(),ActivityLihatData.class);
+                intent = new Intent(getApplicationContext(), ActivityLihatData.class);
                 intent.putExtras(bundle);
                 startActivity(intent);
                 break;
             case R.id.mnnomor:
-                Toast.makeText(getApplicationContext(),"",
-                        Toast.LENGTH_LONG).show();
-                break;
+                switch (nama) {
+                    case "Inayah":
+                        Toast.makeText(getApplicationContext(), "081234567891",
+                                Toast.LENGTH_LONG).show();
+                        break;
+                    case "Ilham":
+                        Toast.makeText(getApplicationContext(), "081234567892",
+                                Toast.LENGTH_LONG).show();
+                        break;
+                    case "Eris":
+                        Toast.makeText(getApplicationContext(), "081234567893",
+                                Toast.LENGTH_LONG).show();
+                        break;
+                    case "Fikri":
+                        Toast.makeText(getApplicationContext(), "081234567894",
+                                Toast.LENGTH_LONG).show();
+                        break;
+                    case "Maul":
+                        Toast.makeText(getApplicationContext(), "081234567895",
+                                Toast.LENGTH_LONG).show();
+                        break;
+                    case "Intan":
+                        Toast.makeText(getApplicationContext(), "081234567896",
+                                Toast.LENGTH_LONG).show();
+                        break;
+                    case "Vina":
+                        Toast.makeText(getApplicationContext(), "081234567897",
+                                Toast.LENGTH_LONG).show();
+                        break;
+                    case "Gita":
+                        Toast.makeText(getApplicationContext(), "081234567898",
+                                Toast.LENGTH_LONG).show();
+                        break;
+                    case "Lutfi":
+                        Toast.makeText(getApplicationContext(), "081234567899",
+                                Toast.LENGTH_LONG).show();
+                        break;
+                    case "Vian":
+                        Toast.makeText(getApplicationContext(), "081234567890",
+                                Toast.LENGTH_LONG).show();
+                        break;
+                }
+                return false;
         }
         return false;
     }
+
 }
